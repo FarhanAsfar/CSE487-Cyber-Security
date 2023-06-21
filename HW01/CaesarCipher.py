09acd851b6fde46321d37986e25d5652200d26a4
Arnob <farhanasfar21@gmail.com>
2023-06-22 02:42:26 +0600

    edit code

--- HW01/CaesarCipher.py
+++ HW01/CaesarCipher.py
@@ -1,24 +1,23 @@
-# The Encryption Function
 def cipher_encrypt(plain_text, key):
 
     encrypted = ""
 
     for c in plain_text:
 
-        if c.isupper(): #check if it's an uppercase character
+        if c.isupper():
 
             c_index = ord(c) - ord('A')
 
-            # shift the current character by key positions
+
             c_shifted = (c_index + key) % 26 + ord('A')
 
             c_new = chr(c_shifted)
 
             encrypted += c_new
 
-        elif c.islower(): #check if its a lowecase character
+        elif c.islower():
 
-            # subtract the unicode of 'a' to get index in [0-25) range
+
             c_index = ord(c) - ord('a')
 
             c_shifted = (c_index + key) % 26 + ord('a')
@@ -28,20 +27,16 @@ def cipher_encrypt(plain_text, key):
             encrypted += c_new
 
         elif c.isdigit():
-
-            # if it's a number,shift its actual value
             c_new = (int(c) + key) % 10
 
             encrypted += str(c_new)
 
         else:
-
-            # if its neither alphabetical nor a number, just leave it like that
             encrypted += c
 
     return encrypted
 
-# The Decryption Function
+
 def cipher_decrypt(ciphertext, key):
 
     decrypted = ""
@@ -52,7 +47,7 @@ def cipher_decrypt(ciphertext, key):
 
             c_index = ord(c) - ord('A')
 
-            # shift the current character to left by key positions to get its original position
+
             c_og_pos = (c_index - key) % 26 + ord('A')
 
             c_og = chr(c_og_pos)
@@ -71,14 +66,14 @@ def cipher_decrypt(ciphertext, key):
 
         elif c.isdigit():
 
-            # if it's a number,shift its actual value
+
             c_og = (int(c) - key) % 10
 
             decrypted += str(c_og)
 
         else:
 
-            # if its neither alphabetical nor a number, just leave it like that
+
             decrypted += c
 
     return decrypted
