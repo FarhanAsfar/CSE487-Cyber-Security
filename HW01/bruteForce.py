5cc0edb193767ee7649c14d43803e6119fc17cc5
Arnob <farhanasfar21@gmail.com>
2023-06-23 23:19:34 +0600

    add bruteForce code

---
+++ HW01/bruteForce.py
@@ -0,0 +1,31 @@
+def cipher_decrypt_lower(ciphertext, key):
+
+    decrypted = ""
+
+    for c in ciphertext:
+
+        if c.islower():
+
+            c_index = ord(c) - ord('a')
+
+            c_og_pos = (c_index - key) % 26 + ord('a')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        else:
+
+            decrypted += c
+
+    return decrypted
+
+
+
+cryptic_text = "xli ehzirxyvi vmhi mr Gerfivve aew ws qygl jyr, Ai aivi ws hvyro ai irhih yt geppmrk"
+
+for i in range(0,26):
+
+    plain_text = cipher_decrypt_lower(cryptic_text, i)
+
+    print("For key {}, decrypted text: {}".format(i, plain_text))
